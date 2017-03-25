276e4f5184c6b6b0630cfb4455211be3e080fa09
DESKTOP-DAL922I\Ethan Rivers <ethan@riversfamily.us>
2017-03-25 18:49:59 -0500

    Again I'm going to make some drastic changes

--- code/btree_it2.h
+++ code/btree_it2.h
@@ -12,124 +12,49 @@ class btree
 {
 	typedef std::size_t size_type;
 
-	struct child;
-
-	struct leaf
+	struct node
 	{
-		leaf(child* n = nullptr): parent(n) {}
-		child* parent;
-		std::vector<T> data;
-	};
-
-	struct child:
-		btree<T>::leaf
-	{
-		child(child* p = nullptr)
+		node(node* p = nullptr)
 		{
 			this->parent = p;
 		}
-		std::vector<leaf*> children;
+		node* parent;
+		std::vector<T> data;
+		std::vector<node*> children;
 	};
 
 public:
 	btree(size_type);
 
 	void insert(const T& data);
-	void insert(leaf* n, const T& data);
-	void insert(child* n, const T& data);
+	void insert(node* n, const T& data);
 
-	void deltree(child*);
-	void deltree(leaf*);
+	void deltree(node*);
 
 	void print() const;
-	void print(leaf*) const;
-	void print(child*) const;
+	void print(node*) const;
 
 	void print_histogram() const;
-	void print_histogram(leaf*) const;
-	void print_histogram(child*) const;
+	void print_histogram(node*) const;
+
+	size_type size() const;
+	size_type size(node*) const; 
 
 	~btree();
 private:
-	void split_node(leaf* n);
-	void split_node(child* n);
+	void node_split(node*);
 	const size_type kmin;
 	const size_type kmax;
 	const size_type cmin;
 	const size_type cmax;
-	child* root;
+	node* root;
 };
 
 template<class T>
 btree<T>::btree(typename btree<T>::size_type m):
 	kmin((m-1)/2), kmax(m-1), cmin(m/2), cmax(m), root(nullptr)
 {
-	root = new child;
-}
-
-template<class T>
-void btree<T>::insert(const T& data)
-{
-	std::cout << "\"Top level insert\"\n";
-	insert(this->root, data);
-}
-
-template<class T>
-void btree<T>::insert(typename btree<T>::leaf* n, const T& data)
-{
-	std::cout << "\"Leaf insert\"\n";
-	if(n != nullptr)
-	{
-		std::cout << -1;
-		if(n->data.size() == 0)
-		{
-			std::cout << 0;
-			n->data.push_back(data);
-		}
-		else
-		{
-			std::cout << 1;
-			typename std::vector<T>::iterator i = std::lower_bound(n->data.begin(), n->data.end(), data);
-			n->data.insert(i, data);
-		}
-		std::cout << 2;
-		std::cout << "Leaf pre-split: ";
-		print(n);
-		split_node(n);
-	}
-}
-
-template<class T>
-void btree<T>::insert(typename btree<T>::child* n, const T& data)
-{
-	std::cout << "\"Child insert\"\n";
-	if(n != nullptr)
-	{
-		if(n->children.size() == 0 && n->data.size() == 0)
-		{
-			std::cout << "Child nodes empty";
-			leaf *a;
-			a = new leaf;
-			a->parent = n;
-
-			n->children.push_back(a);
-			insert(*(n->children.begin()), data);
-		}
-		else if(n->children.size() == 1)
-		{
-			std::cout << "Child nodes have one position";
-			insert(*(n->children.begin()), data);
-		}
-		else
-		{
-			std::cout << "Child ndoes have " << n->children.size() << " positions\n";
-			typename std::vector<T>::iterator i = std::lower_bound(n->data.begin(), n->data.end(), data);
-			auto index = std::distance(n->data.begin(), i);
-			std::cout << "Inserting at child index: " << index << '\n';
-			std::cout << "Index child " << index << " has " << n->children[index]->data.size() << " data pieces\n";
-			insert(*(n->children.begin()+index), data);
-		}
-	}
+	root = new node;
 }
 
 template<class T>
@@ -139,168 +64,218 @@ btree<T>::~btree()
 }
 
 template<class T>
-void btree<T>::deltree(typename btree<T>::child* n)
+void btree<T>::deltree(typename btree<T>::node* n)
 {
 	if(n != nullptr)
 	{
-		for (typename std::vector<leaf*>::iterator i = n->children.begin(); i != n->children.end(); ++i)
+		if(n->children.size() == 0)
 		{
-			deltree(*i);
+			delete n;
 		}
-		delete n;
+		else
+		{
+			for(typename std::vector<node*>::iterator i = n->children.begin(); i != n->children.end(); ++i)
+			{
+				deltree(*i);
+			}
+			delete n;
+		}
 	}
 }
 
 template<class T>
-void btree<T>::deltree(typename btree<T>::leaf* n)
+void btree<T>::insert(const T& data)
 {
-	if(n != nullptr)
-		delete n;
+	insert(this->root, data);
 }
 
 template<class T>
-void btree<T>::split_node(typename btree<T>::leaf* n)
+void btree<T>::insert(typename btree<T>::node* n, const T& data)
 {
 	if(n != nullptr)
 	{
-		if(n->parent == nullptr)
+		// this means it is a leaf node
+		if(n->children.size() == 0)
 		{
-			std::cout << "\"Leaf split\"\t\"Create new parent for leaf\"\n";
-			child* p = new child;
-			n->parent = p;
-			p->data.push_back(*(n->data.end()));
-			p->children.push_back(n);
+			if(n->data.size() == 0)
+				n->data.push_back(data);
+			else
+			{
+				typename std::vector<T>::iterator i = std::lower_bound(n->data.begin(), n->data.end(), data);
+				n->data.insert(i, data);
+			}
+
+			// split node
+			node_split(n);
 		}
-		if(n->data.size() > kmax)
+		// this means it is a child node
+		else
 		{
-			std::cout << "\"Leaf split\"\n";
-			std::cout << "\"Leaf split\"\t\"Create new leaf\"\n";
-			leaf* b = new leaf;
-			b->parent = n->parent;
+			typename std::vector<T>::iterator i = std::lower_bound(n->data.begin(), n->data.end(), data);
+			auto index = std::distance(n->data.begin(), i);
 
-			std::cout << "\"Leaf split\"\t\"Generate median key to pass up\"\n";
-			T midkey = *(n->data.begin()+kmin);
-			
-			std::cout << "\"Leaf split\"\t\"Pushing second half of data to new leaf\"\n";
-			for (typename std::vector<T>::iterator i = n->data.begin()+kmin+1; i != n->data.end(); ++i)
-			{
-				b->data.push_back(*i);
-			}
-			n->data.erase(n->data.begin()+kmin+1, n->data.end());
+			insert(*(n->children.begin()+index), data);
+		}
+	}
+}
 
-
-			std::cout << "\"Leaf split\"\t\"Getting vector index for passed up median key\"\n";
-			typename std::vector<T>::iterator it = std::lower_bound(n->parent->data.begin(), n->parent->data.end(), midkey);
-
-			std::cout << "\"Leaf split\"\t\"Insert new middle key into parent\"\n";
-			std::cout << "\"Leaf split\"\n\"Size of parent data: " << n->parent->data.size() << "\"\n\"Size of parent children:" << n->parent->children.size() << "\"\n";
-			int index = std::distance(n->parent->data.begin(), it);
-			n->parent->data.insert(it, midkey);
-			std::cout << "Parent keys:\t\t";
-			for (typename std::vector<T>::iterator i = n->parent->data.begin(); i != n->parent->data.end(); ++i)
-			{
-				std::cout << *i << ' ';
-			}
-			std::cout << '\n' << "Left leaf keys:\t\t";
-			for (typename std::vector<T>::iterator i = n->data.begin(); i != n->data.end(); ++i)
-			{
-				std::cout << *i << ' ';
-			}
-			std::cout << '\n' << "Right leaf keys:\t";
-			for (typename std::vector<T>::iterator i = b->data.begin(); i != b->data.end(); ++i)
-			{
-				std::cout << *i << ' ';
-			}
+template<class T>
+void btree<T>::node_split(typename btree<T>::node* n)
+{
+	if(n != nullptr)
+	{
+		// this means it is a leaf node
+		if (n->children.size() == 0)
+		{
+			std::cout << "Leaf split:\n";
+			std::cout << "Before split: ";
+			print(n);
 			std::cout << '\n';
-			//if(it < n->parent->data.end())\
+			// check if keys are actually overflowing
+			if(n->data.size() > kmax)
 			{
-				std::cout << "\"Leaf split\"\t\"Inserting index of second half leaf to parent at index: " << index << "\"\n";
-				n->parent->children.insert(n->parent->children.begin()+index+1, b);
-			/*}
-			else
-			{
-				std::cout << "\"Leaf split\"\t\"Inserting index of second half leaf to parent at end\"\n";
-				n->parent->children.push_back(b);
-			}*/
+				// generate new node for second data half
+				node* b = new node;
 
-			std::cout << "\"Leaf split\"\t\"Call split on parent\"\n";
-			split_node(n->parent);
-		}
-	}
-}
+				// get middle key to pass up
+				T midkey = *(n->data.begin()+kmin);
 
-template<class T>
-void btree<T>::split_node(typename btree<T>::child* n)
-{
-	if(n != nullptr)
-	{
-		std::cout << "\"Child split\"\n";
-		if(n->data.size() > kmax)
-		{
-			std::cout << "\"Child split\"\t\"Create second child to hold once split\"\n";
-			child* b = new child;
-
-			std::cout << "\"Child split\"\t\"Checking if current child has a parent\"\n";
-			if(n == this->root)
-			{
-				std::cout << "\"Child split\"\t\"Create new parent child\"\n";
-				child* newch = new child;
-
-				n->parent = newch;
-
-				std::cout << "\"Child split\"\t\"Check if current child is root\"\n";
-				std::cout << "\"Child split\"\t\"Set root to newly created child\"\n";
-				this->root = newch;
-			}
-
-			b->parent = n->parent;
-
-			std::cout << "\"Child split\"\t\"Store new middle key for parent\"\n";
-			T midkey = *(n->data.begin()+kmin);
-
-			std::cout << "\"Child split\"\t\"Split keys and store in new node\"\n";
-			for(typename std::vector<T>::iterator i = n->data.begin()+kmin+1; i != n->data.end(); ++i)
-			{
-				b->data.push_back(*i);
-			}
-			n->data.erase(n->data.begin()+kmin, n->data.end());
-
-			std::cout << "\"Child split\"\t\"Split child nodes and store in new node\"\n";
-			for(auto i = n->children.begin()+cmin; i != n->children.end(); ++i)
-			{
-				b->children.push_back(*i);
-			}
-			n->children.erase(n->children.begin()+cmin, n->children.end());
-
-			if(n->parent->data.size() == 0 && n->parent->children.size() == 0)
-			{
-				n->parent->data.push_back(midkey);
-				n->parent->children.push_back(n);
-				n->parent->children.push_back(b);
-			}
-			else
-			{
-				std::cout << "\"Child split\"\t\"Get index for insertion of middle key to parent\"\n";
-				typename std::vector<T>::iterator lb = std::lower_bound(n->parent->data.begin(), n->parent->data.end(), midkey);
-	
-				std::cout << "\"Child split\"\t\"Insert middle key to parent\"\n";
-				n->parent->data.insert(lb, midkey);
-				std::cout << "\"Child split\"\t\"Check if lb is outside the range of the parent data set\"\n";
-				if(lb != n->parent->data.end())
+				// get second half of data and push to new node
+				for (typename std::vector<T>::iterator i = n->data.begin()+kmin; i != n->data.end(); ++i)
 				{
-					std::cout << "\"Child split\"\t\"Get index for node insertion\"\n";
-					int index = std::distance(n->parent->data.begin(), lb);
-					std::cout << "\"Child split\"\t\"Inserting index of second half child to parent at index: " << index << "\"\n";
+					b->data.push_back(*i);
+				}
+				// clear second half data from original node
+				n->data.erase(n->data.begin()+kmin, n->data.end());
+
+				// check if parent exists
+				if(n->parent != nullptr)
+				{
+					std::cout << "Here! 0\n";
+					// get index for middle key insertion of parent
+					typename std::vector<T>::iterator i = std::lower_bound(n->parent->data.begin(), n->parent->data.end(), midkey);
+					std::cout << "Here! 1\n";
+					// get index for node insertion
+					auto index = std::distance(n->parent->data.begin(), i);
+					std::cout << "Here! 2\n";
+					// insert mid key into position at parent
+					n->parent->data.insert(i, midkey);
+					std::cout << "Here! 3\n" << n->parent->children.size() << ' ' << index+1;
+					// insert node pointer of new node into parent at position
 					n->parent->children.insert(n->parent->children.begin()+index, b);
+					std::cout << "Here! 4\n";
+					b->parent = n->parent;
 				}
 				else
 				{
-					std::cout << "\"Child split\"\t\"Inserting index of second half child to parent at end\"\n";
-					n->parent->children.push_back(b);
-				}
-			}
+					// create new parent node
+					node* p = new node;
+					// add mid key as first key to new parent
+					p->data.push_back(midkey);
+					// push original node and second half node to new parent
+					p->children.push_back(n);
+					p->children.push_back(b);
 
-			split_node(n->parent);
+					// change parents of nodes
+					n->parent = p;		// original
+					b->parent = p;		// new (second half node)
+
+					// update btree root node
+					if(n == this->root)
+						this->root = p;
+				}
+
+				std::cout << "Left after split: ";
+				print(n);
+				std::cout << "\nRight after split: ";
+				print(b);
+				std::cout << '\n';
+
+				// call split on parent
+				node_split(n->parent);
+			}
+		}
+		// this means it is a child node
+		else
+		{
+			std::cout << "Child split:\n";
+			// check if keys are actually overflowing
+			std::cout << "Node key count: " << n->data.size() << '\n';
+			std::cout << "Node child count: " << n->children.size() << '\n';
+			if(n->data.size() > kmax)
+			{
+				std::cout << "Here! 0\n";
+				// generate new node to hold split keys/node pointers
+				node* b = new node;
+
+				std::cout << "Here! 1\n";
+				// get midkey to pass up to parent
+				T midkey = *(n->data.begin()+kmin);
+
+				std::cout << "Here! 2\n";
+				// get second half of keys, but exclude midkey from copy
+				for (typename std::vector<T>::iterator i = n->data.begin()+kmin+1; i != n->data.end(); ++i)
+				{
+					b->data.push_back(*i);
+				}
+				std::cout << "Here! 3\n";
+				// erase second half of keys from original node including mid key
+				n->data.erase(n->data.begin()+kmin, n->data.end());
+
+				std::cout << "Here! 4\n";
+				// copy second half of node pointers to new node
+				for (typename std::vector<node*>::iterator i = n->children.begin()+cmin; i != n->children.end(); ++i)
+				{
+					b->children.push_back(*i);
+				}
+				std::cout << "Here! 5\n";
+				// erase second half of node pointers from original node
+				n->children.erase(n->children.begin()+cmin, n->children.end());
+
+				// check if parent exists
+				if(n->parent != nullptr)
+				{
+					std::cout << "Here! 6\n";
+					// get index of parent to insert mid key at
+					std::cout << n->parent->data.size() << '\n';
+					typename std::vector<T>::iterator i = std::lower_bound(n->parent->data.begin(), n->parent->data.end(), midkey);
+					std::cout << "Here! 7\n";
+					// get index for node insertion
+					auto index = std::distance(n->parent->data.begin(), i);
+					std::cout << "Here! 8\n";
+					// insert key into parent
+					n->parent->data.insert(i, midkey);
+					std::cout << "Here! 9\n";
+					// insert node into parent
+					std::cout << n->parent->data.size() << ' ' << n->parent->children.size() << ' ' << index+1;
+					n->parent->children.insert(n->parent->children.begin()+index+1, b);
+				}
+				else
+				{
+					std::cout << "Here! 10\n";
+					// create new parent node
+					node* p = new node;
+					std::cout << "Here! 11\n";
+					// push midkey to parent as new node
+					p->data.push_back(midkey);
+					std::cout << "Here! 12\n";
+					// push nodes to parent
+					p->children.push_back(n);	// original
+					p->children.push_back(b);	// new (second half)
+
+					// set new parent as parent of original and half node
+					n->parent = p;
+					b->parent = p;
+
+					// update btree root node
+					if(n == this->root)
+						std::cout << "Here! 13\n";
+						this->root = p;
+				}
+
+				// call split on parent
+				node_split(n->parent);
+			}
 		}
 	}
 }
@@ -312,26 +287,23 @@ void btree<T>::print() const
 }
 
 template<class T>
-void btree<T>::print(typename btree<T>::leaf* n) const
+void btree<T>::print(typename btree<T>::node* n) const
 {
 	if(n != nullptr)
 	{
-		for (typename std::vector<T>::iterator i = n->data.begin(); i != n->data.end(); ++i)
+		if(n->children.size() == 0)
 		{
-			std::cout << *i << ' ';
+			for(typename std::vector<T>::iterator i = n->data.begin(); i != n->data.end(); ++i)
+			{
+				std::cout << *i << ' ';
+			}
 		}
-	}
-	std::cout << '\n';
-}
-
-template<class T>
-void btree<T>::print(typename btree<T>::child* n) const
-{
-	if(n != nullptr)
-	{
-		for (typename std::vector<leaf*>::iterator i = n->children.begin(); i != n->children.end(); ++i)
+		else
 		{
-			print(*i);
+			for(typename std::vector<node*>::iterator i = n->children.begin(); i != n->children.end(); ++i)
+			{
+				print(*i);
+			}
 		}
 	}
 }
@@ -343,21 +315,58 @@ void btree<T>::print_histogram() const
 }
 
 template<class T>
-void btree<T>::print_histogram(typename btree<T>::leaf* n) const
+void btree<T>::print_histogram(typename btree<T>::node* n) const
 {
-	for (typename std::vector<T>::iterator i = n->data.begin(); i != n->data.end(); ++i)
+	if(n != nullptr)
 	{
-		std::cout << 'x' << ' ';
+		if(n->children.size() == 0)
+		{
+			std::cout << "Node: ";
+			for(typename std::vector<T>::iterator i = n->data.begin(); i != n->data.end(); ++i)
+			{
+				std::cout << "x ";
+			}
+			std::cout << '\n';
+		}
+		else
+		{
+			for(typename std::vector<node*>::iterator i = n->children.begin(); i != n->children.end(); ++i)
+			{
+				print_histogram(*i);
+			}
+		}
 	}
-	std::cout << '\n';
 }
 
 template<class T>
-void btree<T>::print_histogram(typename btree<T>::child* n) const
+typename btree<T>::size_type btree<T>::size() const
 {
-	for (auto i = n->children.begin(); i != n->children.end(); ++i)
+	return size(this->root);
+}
+
+template<class T>
+typename btree<T>::size_type btree<T>::size(typename btree<T>::node* n) const
+{
+	if(n != nullptr)
 	{
-		print_histogram(*i);
+		typename btree<T>::size_type count = 0;
+		if(n->children.size() == 0)
+		{
+			for(typename std::vector<T>::iterator i = n->data.begin(); i != n->data.end(); ++i)
+			{
+				++count;
+			}
+		}
+		else
+		{
+			for(typename std::vector<node*>::iterator i = n->children.begin(); i != n->children.end(); ++i)
+			{
+				count += size(*i);
+			}
+		}
+
+		return count;
 	}
 }
+
 #endif
--- code/btree_test.cpp
+++ code/btree_test.cpp
@@ -5,14 +5,18 @@
 int main()
 {
 	btree<int> b(3);
+	int k;
 	for(int i = 0; i < 100; ++i)
 	{
 		std::cout << "\nTest " << i << '\n';
-		b.insert(i);
+		k = std::rand();
+		std::cout << "Inserting " << k << " into tree\n";
+		b.insert(k);
 		std::cout << "Data in tree: \n";
 		b.print();
-		std::cout << "Tree histogram: \n";
+		std::cout << "\nTree histogram: \n";
 		b.print_histogram();
+		std::cout << "\nSize of tree: \n" << b.size() << '\n';
 	}
 
 	b.print();
